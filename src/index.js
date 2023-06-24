2f46ff3586b83be14cb46664dceb4319b114c67d
Pumpon4yk <tt10392@gmail.com>
2023-06-24 12:43:49 +0200

    r

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
     {/* <PersistGate loading={null} persistor={persistor}> */}
-    <BrowserRouter basename="/project-Bra1n_Gain">
+    <BrowserRouter basename="/repo-test-TP">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
